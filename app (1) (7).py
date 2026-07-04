fa9f425a8948f1ba98e772303f5bc4a9821e0fe2
pawarpranay2010-sketch <pawarpranay2010@gmail.com>
2026-07-04 13:47:46 +0530

    Update app (1) (7).py

--- app (1) (7).py
+++ app (1) (7).py
@@ -6,6 +6,9 @@ import json
 import pandas as pd
 from docx import Document
 from pypdf import PdfReader
+from reportlab.platypus import SimpleDocTemplate, Paragraph
+from reportlab.lib.styles import getSampleStyleSheet
+from reportlab.lib.pagesizes import letter
 
 # Set Page Config for mobile
 st.set_page_config(page_title="Financial Timeline Engine", layout="centered")
